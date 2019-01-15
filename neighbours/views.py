9ca6d3dd8bd7951879909d630b635764abcf5b3a
RKatana <roduor41@gmail.com>
2019-01-15 14:47:28 +0300

    added the view to handle sign up

--- neighbours/views.py
+++ neighbours/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import RegistrationForm,ProfileForm,BusinessForm
+from .models import Occupants,Neighborhood,Business
 
 # Create your views here.
+def signup(request):
+    if request.method =='POST':
+        form=RegistrationForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('/')
+    else:
+        form= RegistrationForm()
+    return render(request,'registration/registration_form.html',{'form':form})
+
